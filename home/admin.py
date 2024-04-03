48027da657e4abc586fcb8a5588f469aa4f0b02e
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-04-03 15:42:50 +0100

    "Admin Changes"

--- home/admin.py
+++ home/admin.py
@@ -1,18 +1,12 @@
 from django.contrib import admin
-# from .models import PaymentNotification
+from .models import PaymentNotification
 
 
-# @admin.register(PaymentNotification)
-# class PaymentNotificationAdmin(admin.ModelAdmin):
-#     list_display = ('tx_id', 'merchant_tx_id', 'amount', 'currency', 'status', 'data')
-#     search_fields = ('tx_id', 'merchant_tx_id', 'status')
-#     readonly_fields = ('data',)
-#
-#     def has_add_permission(self, request):
-#         return False  # To prevent manual entries since these should only come from notifications
-#
-#     def has_change_permission(self, request, obj=None):
-#         return False  # Optional: to prevent editing of notifications
-#
-#     def has_delete_permission(self, request, obj=None):
-#         return True  # Depending on whether you want to allow deletion
+class PaymentNotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        'tx_id', 'amount', 'acquirer_tx_id', 'auth_code', 'merchant_id', 'brand_id', 'merchant_tx_id', 'action',
+        'payment_solution_id', 'status', 'original_tx_id')
+    search_fields = ('tx_id', 'merchant_tx_id', 'customer_id', 'pan', 'status')
+
+
+admin.site.register(PaymentNotification, PaymentNotificationAdmin)
