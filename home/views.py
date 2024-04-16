52c9a354ff324dc396f6ba51b82200981a1b7e52
morgan360 <morgan.mcknight@gmail.com>
2024-04-16 09:37:42 +0000

    Changes on production

--- home/views.py
+++ home/views.py
@@ -27,11 +27,7 @@ BOIPA_MERCHANT_ID = os.getenv('BOIPA_MERCHANT_ID')
 BOIPA_PASSWORD = os.getenv('BOIPA_PASSWORD')
 BOIPA_TOKEN_URL = os.getenv('BOIPA_TOKEN_URL')  # URL to obtain the session token
 PAYMENT_FORM_URL = os.getenv('HPP_FORM')  # URL for the payment form
-NGROK = os.getenv('NGROK')  # For ip tunnel from BOIPA
 HPP_FORM = os.getenv('HPP_FORM')
-base_order_id = "TCSP005"
-timestamp = time.strftime("%Y%m%d%H%M%S")
-order_id = f"{base_order_id}_{timestamp}"
 
 
 def home(request):
@@ -40,6 +36,10 @@ def home(request):
 
 
 def get_boipa_session_token():
+    base_order_id = "TCSP006"
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    order_id = f"{base_order_id}_{timestamp}"
+
     url = BOIPA_TOKEN_URL  # UAT URL, change for production
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     payload = {
@@ -47,14 +47,14 @@ def get_boipa_session_token():
         "password": settings.BOIPA_PASSWORD,
         "action": "AUTH",  # Based on the operation you're performing
         "timestamp": int(time.time() * 1000),  # Current time in milliseconds
-        "allowOriginUrl": NGROK,  # Your ngrok URL for CORS
+        "allowOriginUrl": 'https://morganmck.eu.pythonanywhere.com/',  # Your ngrok URL for CORS
         "channel": "ECOM",
         "country": "IE",  # Example country code
         "currency": "EUR",  # Example currency
-        "amount": "100.00",  # Example amount for AUTH or PURCHASE
+        "amount": "190.00",  # Example amount for AUTH or PURCHASE
         "merchantTxId": order_id,  # Your internal order ID
-        "merchantLandingPageUrl": NGROK + "/payment-response/",  # General callback URL for customer redirection
-        "merchantNotificationUrl": NGROK + "/payment-notification/",  # Server-to-server notification URL(important
+        "merchantLandingPageUrl": "https://morganmck.eu.pythonanywhere.com//payment-response/",  # General callback URL for customer redirection
+        "merchantNotificationUrl":"https://morganmck.eu.pythonanywhere.com//payment-notification/",  # Server-to-server notification URL(important
         # in case user makes a mess)
         "merchantLandingPageRedirectMethod": "GET",  # Ensure redirects use GET
     }
@@ -123,7 +123,6 @@ def payment_notification(request):
         # Parse the URL-encoded form data
         data = QueryDict(request.body)
 
-        # Now, you can access the values using the same dictionary-like interface
         country = data.get('country')
         amount = data.get('amount')
         txId = data.get('txId')
@@ -131,9 +130,8 @@ def payment_notification(request):
         status = data.get('status')
 
         # Proceed with your logic
-
-        # You can adjust the fields based on what's most relevant to your needs
         # Store the collected data in the database
+        # Uncomment the following line and make sure the PaymentNotification model is imported
         PaymentNotification.objects.create(
             country=country,
             amount=amount,
@@ -141,3 +139,10 @@ def payment_notification(request):
             merchantTxId=merchantTxId,
             status=status,
         )
+
+        # Return a successful HTTP response
+        return HttpResponse('Payment processed successfully', status=200)
+
+    # If not a POST request, or if other issues are encountered
+    return HttpResponse('Invalid request', status=400)
+
