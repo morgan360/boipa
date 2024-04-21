3970b80fa4cb8b23b1c9f5a3a1192bdb995812e5
Morgan McKnight <69460926+morgan360@users.noreply.github.com>
2024-04-21 20:21:33 +0100

    "Admin Changes"

--- home/payment_functions.py
+++ home/payment_functions.py
@@ -8,11 +8,12 @@ import logging
 
 payments_logger = logging.getLogger('payments')
 
-def get_boipa_session_token(order_id, total_price):
+
+def get_boipa_session_token(request, order_id, total_price):
     try:
         order = SimpleOrder.objects.get(id=order_id)
         amount = Decimal(f"{total_price:.2f}")
-
+        ip_address=get_client_ip(request)
         order_ref = f'simple_{order_id}'
 
         url = settings.BOIPA_TOKEN_URL
@@ -32,6 +33,7 @@ def get_boipa_session_token(order_id, total_price):
             "merchantNotificationUrl": settings.NGROK + reverse('home:payment-notification'),
             "merchantLandingPageRedirectMethod": "GET",
             "userDevice": "DESKTOP",
+            "customerIPAddress": ip_address,
             "merchantChallengeInd": "01",
             "merchantDecReqInd": "N",
             "freeText": "Optional extra transaction info",
@@ -43,7 +45,8 @@ def get_boipa_session_token(order_id, total_price):
         if response.status_code == 200:
             return response.json().get('token')
         else:
-            payments_logger.error("Error obtaining session token: HTTP Status %s: %s", response.status_code, response.text)
+            payments_logger.error("Error obtaining session token: HTTP Status %s: %s", response.status_code,
+                                  response.text)
             return None
     except requests.RequestException as e:
         payments_logger.error("Network-related error when obtaining session token: %s", str(e))
@@ -51,3 +54,12 @@ def get_boipa_session_token(order_id, total_price):
     except Exception as e:
         payments_logger.error("Unexpected error when obtaining session token: %s", str(e))
         return None
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
